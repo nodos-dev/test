759b6eeccb074a5f00ead4c53a24ca47e5ad178a
M. Samil Atesoglu <samil.atesoglu@nodos.dev>
2025-07-10 19:10:28 +0300

    Add Assert node

---
+++ Source/Nodes/AssertNode.cpp
@@ -0,0 +1,61 @@
+#include <Nodos/PluginHelpers.hpp>
+#include "Generated/Test_generated.h"
+
+namespace nos::test
+{
+
+struct AssertNode : NodeContext
+{
+	using NodeContext::NodeContext;
+
+private:
+	bool LastCondition = true; // Track previous condition state
+	bool StatusInitialized = false; // Track if status was ever set
+	uint32_t FrameCount = 0;
+
+public:
+
+	void OnPathStart() override
+	{
+		LastCondition = true;
+		StatusInitialized = false;
+		FrameCount = 0;
+		ClearNodeStatusMessages();
+	}
+
+	nosResult ExecuteNode(nosNodeExecuteParams* params) override
+	{
+		nos::NodeExecuteParams pins(params);
+		auto& condition = *pins.GetPinData<bool>(NOS_NAME("Condition"));
+		auto& behaviour = *pins.GetPinData<AssertionBehaviour>(NOS_NAME("Behaviour"));
+		auto& framesToWait = *pins.GetPinData<uint32_t>(NOS_NAME("NumFramesToWait"));
+		++FrameCount;
+		nosEngine.SetPinValueByName(NodeId, NOS_NAME("FrameCount"), nos::Buffer::From(FrameCount));
+		if (FrameCount < framesToWait)
+			return NOS_RESULT_SUCCESS; // Wait for the specified number of frames
+		switch (behaviour)
+		{
+		case AssertionBehaviour::EXIT_WITH_STATUS_CODE:
+			exit(condition ? 0 : 1);
+			break;
+		case AssertionBehaviour::SHOW_ON_NODE_STATUS:
+			if (!StatusInitialized || condition != LastCondition)
+			{
+				SetNodeStatusMessage(
+					condition ? "Assertion passed" : "Assertion failed",
+					condition ? fb::NodeStatusMessageType::INFO : fb::NodeStatusMessageType::FAILURE);
+				LastCondition = condition;
+				StatusInitialized = true;
+			}
+			break;
+		}
+		return NOS_RESULT_SUCCESS;
+	}
+};
+
+nosResult RegisterAssertNode(nosNodeFunctions* funcs)
+{
+	NOS_BIND_NODE_CLASS(NOS_NAME_STATIC("Assert"), AssertNode, funcs);
+	return NOS_RESULT_SUCCESS;
+}
+}
---
+++ Source/TestMain.cpp
@@ -0,0 +1,56 @@
+// Copyright MediaZ Teknoloji A.S. All Rights Reserved.
+
+// Includes
+#include <Nodos/PluginHelpers.hpp>
+
+NOS_INIT()
+
+NOS_BEGIN_IMPORT_DEPS()
+NOS_END_IMPORT_DEPS()
+
+namespace nos::test
+{
+
+enum Nodes : int
+{
+	AssertNode = 0,
+	Count
+};
+
+nosResult RegisterAssertNode(nosNodeFunctions*);
+
+nosResult NOSAPI_CALL ExportNodeFunctions(size_t* outSize, nosNodeFunctions** outList)
+{
+	*outSize = Nodes::Count;
+	if (!outList)
+		return NOS_RESULT_SUCCESS;
+
+#define GEN_CASE_NODE(name)					\
+	case Nodes::name: {					\
+		auto ret = Register##name(node);	\
+		if (NOS_RESULT_SUCCESS != ret)		\
+			return ret;						\
+		break;								\
+	}
+
+	for (int i = 0; i < Nodes::Count; ++i)
+	{
+		auto node = outList[i];
+		switch ((Nodes)i) {
+		default:
+			break;
+			GEN_CASE_NODE(AssertNode)
+		}
+	}
+	return NOS_RESULT_SUCCESS;
+}
+
+extern "C"
+{
+NOSAPI_ATTR nosResult NOSAPI_CALL nosExportPlugin(nosPluginFunctions* out)
+{
+	out->ExportNodeFunctions = ExportNodeFunctions;
+	return NOS_RESULT_SUCCESS;
+}
+}
+}	
