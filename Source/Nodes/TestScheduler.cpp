027de14de21590b5a19a19b1ca60841e014afda5
Caner Coskun <caner.coskun@nodos.dev>
2025-12-02 16:11:23 +0300

    Fix tests not passing since utilities is not present (#1)
    * Fix tests not passing since utilities is not present
    
    This was caused by a cyclic test dependency, since utilities tests depends on the test plugin, it cannot be released, and test plugins test depends on utilities, so it cannot be released too.
    This commit introduces a new node called 'TestScheduler' to be used in simple testing cases, which replaces Sink node in the Assert_True.nosa test graph, removing the utilities dependency.

---
+++ Source/Nodes/TestScheduler.cpp
@@ -0,0 +1,63 @@
+#include <Nodos/Plugin.hpp>
+
+namespace nos::test
+{
+
+NOS_REGISTER_NAME(FreeRun)
+NOS_REGISTER_NAME(DeltaTime)
+
+struct TestScheduler : NodeContext
+{
+	nosResult OnCreate(nosFbNodePtr node) override
+	{
+		AddPinValueWatcher(NSN_DeltaTime, [this](nos::Buffer const& newVal, std::optional<nos::Buffer> oldValue) {
+			nosEngine.RecompilePath(NodeId);
+			});
+		AddPinValueWatcher(NSN_FreeRun, [this](nos::Buffer const& newVal, std::optional<nos::Buffer> oldValue) {
+			nosEngine.RecompilePath(NodeId);
+			});
+		return NOS_RESULT_SUCCESS;
+	}
+
+	void ScheduleNode()
+	{
+		nosScheduleNodeParams scheduleParams
+		{
+			.NodeId = NodeId,
+			.AddScheduleCount = 1
+		};
+		nosEngine.ScheduleNode(&scheduleParams);
+	}
+
+	void OnPathStart() override
+	{
+		ScheduleNode();
+	}
+
+	nosResult ExecuteNode(nosNodeExecuteParams* params) override
+	{
+		ScheduleNode();
+		return NOS_RESULT_SUCCESS;
+	}
+
+	void GetScheduleInfo(nosScheduleInfo* info)
+	{
+		info->Type = NOS_SCHEDULE_TYPE_ON_DEMAND;
+		if (**GetWatchedPinValue<bool>(NSN_FreeRun))
+		{
+			info->DeltaSeconds = { 0, 0 };
+		}
+		else
+		{
+			auto delta = **GetWatchedPinValue<nosVec2u>(NSN_DeltaTime);
+			info->DeltaSeconds = delta;
+		}
+	}
+};
+
+nosResult RegisterTestScheduler(nosNodeFunctions* nodeFunctions)
+{
+	NOS_BIND_NODE_CLASS(NOS_NAME("TestScheduler"), TestScheduler, nodeFunctions);
+	return NOS_RESULT_SUCCESS;
+}
+}
